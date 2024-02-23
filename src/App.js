0aa31d45606a1cc07318c6b01e6bf333f68d7f36
Zirak <zirakmughal@live.com>
2024-02-23 03:25:27 -0500

    changes to ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import Chat from "./components/chat/Chat";
 import LogoNavbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        width: "100vw",
-        height: "100vh",
-        top: 0,
-        left: 0,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <LogoNavbar />
-      <Chat />
-    </div>
+    <ChakraProvider> {/* Wrap your application with ChakraProvider */}
+      <div
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          top: 0,
+          left: 0,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <LogoNavbar />
+        <Chat />
+      </div>
+    </ChakraProvider>
   );
 }
 
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,85 +4,109 @@ import "./styles.css"; // Your custom dark mode styles
 import {
   MainContainer,
   ChatContainer,
-  MessageInput,
   ExpansionPanel,
   Conversation,
   ConversationList,
   Search,
-  Avatar,
   Sidebar,
   ConversationHeader,
   InfoButton,
 } from "@chatscope/chat-ui-kit-react";
+import {
+  FormControl,
+  FormLabel,
+  Input as DefaultInput,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  ChakraProvider,
+} from "@chakra-ui/react";
+
+// Custom Input component
+const Input = ({ errorMessage, label, helperText, inputLeftAddon, inputRightAddon, required, ...props }) => {
+  return (
+    <FormControl isInvalid={Boolean(errorMessage)} isRequired={required}>
+      {label && <FormLabel>{label}</FormLabel>}
+      <InputGroup alignItems="center">
+        {inputLeftAddon && <InputLeftElement>{inputLeftAddon}</InputLeftElement>}
+        <DefaultInput {...props} />
+        {inputRightAddon && <InputRightElement>{inputRightAddon}</InputRightElement>}
+      </InputGroup>
+    </FormControl>
+  );
+};
 
 function Chat() {
   return (
-    <div className="chat-wrapper">
-      <MainContainer responsive>
-        <Sidebar position="left" scrollable={false} className="sidebar">
-          <Search
-            placeholder="Search..."
-            className="search-bar custom-placeholder"
-          />
-          <ConversationList className="conversation-list">
-            <Conversation
-              name="Lorem"
-              lastSenderName="Lorem"
-              info="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              className="conversation"
-            >
-              {/* <Avatar name="Lilly" /> */}
-            </Conversation>
-            {/* Add more conversations as needed */}
-          </ConversationList>
-        </Sidebar>
-
-        <ChatContainer className="chat-container">
-          <ConversationHeader className="conversation-header">
-            <ConversationHeader.Back />
-            {/* <Avatar name="Zoe" /> */}
-            <ConversationHeader.Content
-              userName="Lorem ipsum"
-              info="Last asked 10 mins ago"
+    <ChakraProvider> {/* Wrap your Chat component with ChakraProvider */}
+      <div className="chat-wrapper">
+        <MainContainer responsive>
+          <Sidebar position="left" scrollable={false} className="sidebar">
+            <Search
+              placeholder="Search..."
+              className="search-bar custom-placeholder"
             />
-            <ConversationHeader.Actions>
-              <InfoButton />
-            </ConversationHeader.Actions>
-          </ConversationHeader>
-          <MessageInput
-            placeholder="Type message here"
-            className="message-input"
-          />
-        </ChatContainer>
+            <ConversationList className="conversation-list">
+              <Conversation
+                name="Lorem"
+                lastSenderName="Lorem"
+                info="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                className="conversation"
+              >
+                {/* <Avatar name="Lilly" /> */}
+              </Conversation>
+            </ConversationList>
+          </Sidebar>
 
-        <Sidebar position="right" className="sidebar">
-          <ExpansionPanel
-            title="HELP"
-            className="expansion-panel"
-            style={{
-              backgroundColor: "#d5e8f6",
-            }}
-          >
-            <p>Documentation or User Manuals</p>
-            {/* Add more content as needed */}
-          </ExpansionPanel>
-          <ExpansionPanel
-            title="OPTIONS"
-            className="expansion-panel"
-            style={{
-              backgroundColor: "#d5e8f6",
-            }}
-          >
-            <p>Check for Updates</p>
-            <p>Contact Customer Support</p>
-            <p>Privacy Policy</p>
-            <p>Release Notes</p>
-            {/* Add more content as needed */}
-          </ExpansionPanel>
-        </Sidebar>
-      </MainContainer>
-    </div>
+          <ChatContainer className="chat-container">
+            <ConversationHeader className="conversation-header">
+            <ConversationHeader.Back />
+              {/* <Avatar name="Zoe" /> */}
+              <ConversationHeader.Content
+                userName="Lorem ipsum"
+                info="Last asked 10 mins ago"
+              />
+              <ConversationHeader.Actions>
+                <InfoButton />
+              </ConversationHeader.Actions>
+            </ConversationHeader>
+            {/* Replace MessageInput with the custom Input component */}
+            <Input placeholder="Type message here" className="message-input" />
+          </ChatContainer>
+
+          <Sidebar position="right" className="sidebar">
+            <ExpansionPanel
+              title="HELP"
+              className="expansion-panel"
+              style={{
+                backgroundColor: "#d5e8f6",
+              }}
+            >
+              <p>Documentation or User Manuals</p>
+              {/* Add more content as needed */}
+            </ExpansionPanel>
+            <ExpansionPanel
+              title="OPTIONS"
+              className="expansion-panel"
+              style={{
+                backgroundColor: "#d5e8f6",
+              }}
+            >
+              <p>Check for Updates</p>
+              <p>Contact Customer Support</p>
+              <p>Privacy Policy</p>
+              <p>Release Notes</p>
+              {/* Add more content as needed */}
+            </ExpansionPanel>
+          </Sidebar>
+        </MainContainer>
+      </div>
+    </ChakraProvider>
   );
 }
 
 export default Chat;
+
+
+
+
